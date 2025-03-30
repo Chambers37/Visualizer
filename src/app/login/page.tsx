3d45266a163f4ee2ff1327380003e759d42d23c0
Chambers37 <103705874+Chambers37@users.noreply.github.com>
2025-03-29 23:43:09 -0400

    Added links for easier navigation during dev

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,16 +1,26 @@
 'use client'
 
 import { startSpotifyAuth } from "@/app/lib/spotify"
+import Link from "next/link"
 
 export default function LoginPage() {
 
     return (
       <div>
-        <button onClick={startSpotifyAuth}
-        className="bg-[rgb(29,180,82)] hover:bg-[rgb(27,163,74)] rounded-full text-xl font-bold py-2 px-4"
-        >
-          Sign in with Spotify
-        </button>
+        <div className="flex justify-center mt-10">
+          <button onClick={startSpotifyAuth}
+          className="bg-[rgb(29,180,82)] hover:bg-[rgb(27,163,74)] rounded-full text-xl font-bold py-2 px-4"
+          >
+            Sign in with Spotify
+          </button>
+        </div>
+        <div className='flex justify-center mt-7'>
+          <div className=' text-3xl bg-blue-500 rounded-full w-[200px] text-center '>
+            <Link  href='/'>
+              Home
+            </Link>
+          </div> 
+        </div>
       </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,18 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div>
-      <p>
-        Visualizer
-      </p>
-      <Link  href='/login'>
-        Login Page
-      </Link>
+      <div className='text-5xl w-full text-center h-[100px] mt-5'>
+        <p>
+          Visualizer
+        </p>
+      </div>
+        <div className='flex justify-center'>
+          <div className=' text-3xl bg-blue-500 rounded-full w-[200px] text-center '>
+            <Link  href='/login'>
+              Login Page
+            </Link>
+          </div> 
+        </div>     
     </div>
   );
 }
